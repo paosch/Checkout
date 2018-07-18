22ad26eb3a3eb968d53691ad4b7e4837c03b20e0
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-18 21:56:16 +0100

    #scan gives a discount when 3 'A' items are scanned

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,11 +2,16 @@ class Checkout
   attr_reader :total
   def initialize
     @total = 0
+    @count_A = 0
   end
   def scan(item)
-    return @total += 50 if item == 'A'
-    return @total += 30 if item == 'B'
-    return @total += 20 if item == 'C'
-    return @total += 15 if item == 'D'
+    if item == 'A'
+      @count_A += 1
+      @total -= 20 if @count_A % 3 == 0
+    end
+    @total += 50 if item == 'A'
+    @total += 30 if item == 'B'
+    @total += 20 if item == 'C'
+    @total += 15 if item == 'D'
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,16 +4,28 @@ describe Checkout do
   it 'starts with total at zero' do
     expect(subject.total).to eq 0
   end
-  it "#scan registers 50 when 1 'A' item is scanned" do
-    expect { subject.scan('A') }.to change { subject.total }.by(50)
+  context 'normal prices' do
+    it "#scan registers 50 when 1 'A' item is scanned" do
+      expect { subject.scan('A') }.to change { subject.total }.by(50)
+    end
+    it "#scan registers 30 when 1 'B' item is scanned" do
+      expect { subject.scan('B') }.to change { subject.total }.by(30)
+    end
+    it "#scan registers 20 when 1 'C' item is scanned" do
+      expect { subject.scan('C') }.to change { subject.total }.by(20)
+    end
+    it "#scan registers 15 when 1 'D' item is scanned" do
+      expect { subject.scan('D') }.to change { subject.total }.by(15)
+    end
+    it "#scan registers 100 when 2 'A' items are scanned" do
+      2.times { subject.scan('A') }
+      expect(subject.total).to eq 100
+    end
   end
-  it "#scan registers 30 when 1 'B' item is scanned" do
-    expect { subject.scan('B') }.to change { subject.total }.by(30)
-  end
-  it "#scan registers 20 when 1 'C' item is scanned" do
-    expect { subject.scan('C') }.to change { subject.total }.by(20)
-  end
-  it "#scan registers 15 when 1 'D' item is scanned" do
-    expect { subject.scan('D') }.to change { subject.total }.by(15)
+  context 'special prices' do
+    it "#scan registers 130 when 3 'A' items are scanned" do
+      3.times { subject.scan('A') }
+      expect(subject.total).to eq 130
+    end
   end
 end
