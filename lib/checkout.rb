b7e9c7cd808ed959d3a86b391333c7b350a58234
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-18 22:06:26 +0100

    Refactored: #scan now uses PRICE hash

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,10 +1,13 @@
 class Checkout
   attr_reader :total
+  PRICE = {'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15}
+
   def initialize
     @total = 0
     @count_A = 0
     @count_B = 0
   end
+
   def scan(item)
     if item == 'A'
       @count_A += 1
@@ -14,9 +17,6 @@ class Checkout
       @count_B += 1
       @total -= 15 if @count_B % 2 == 0
     end
-    @total += 50 if item == 'A'
-    @total += 30 if item == 'B'
-    @total += 20 if item == 'C'
-    @total += 15 if item == 'D'
+    @total += PRICE[item]
   end
 end
